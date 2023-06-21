d2fdcf6defac4e8306ef6c7262b19269215bb316
artur-sak13 <artursak1994@gmail.com>
2023-06-21 10:52:28 -0500

    chore: fix minor eslint callouts

--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,13 @@
-"use strict";
-
 import * as vscode from "vscode";
 import PerlLintProvider from "./features/PerlLintProvider";
 import PerlSyntaxProvider from "./features/PerlSyntaxProvider";
 
 export function activate(context: vscode.ExtensionContext) {
-  let linter = new PerlLintProvider();
+  const linter = new PerlLintProvider();
   linter.activate(context.subscriptions);
-  let checker = new PerlSyntaxProvider();
+
+  const checker = new PerlSyntaxProvider();
   checker.activate(context.subscriptions);
 }
 
-export function deactivate() {}
+export function deactivate() { }
